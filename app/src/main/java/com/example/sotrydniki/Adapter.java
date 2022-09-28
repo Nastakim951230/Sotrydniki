59379493c93ab9abef6cc399ea12e435ba820504
unknown <@ngknn.local>
2022-09-28 15:01:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sotrydniki/Adapter.java
@@ -0,0 +1,33 @@
+package com.example.sotrydniki;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class Adapter extends BaseAdapter {
+
+    private Context nContext;
+    List<Mask>  maskList;
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/sotrydniki/ConnectionHelper.java
@@ -0,0 +1,43 @@
+package com.example.sotrydniki;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionHelper {
+    String userName, userPassword, ip, port, dataBase;
+
+    @SuppressLint("NewApi")
+    public Connection connectionClass()
+    {
+        ip ="ngknn.ru";
+        dataBase ="Trifonova_41P";
+        userPassword="12357";
+        userName="31П";
+        port="1433";
+
+        StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Connection connection=null;
+        String ConnectionURL=null;
+
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" + "databasename=" + dataBase + ";user=" + userName + ";password=" + userPassword + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+
+        }
+
+        catch (Exception ex)
+        {
+            Log.e("Error", ex.getMessage());
+        }
+        return connection;
+    }
+
+
+}
